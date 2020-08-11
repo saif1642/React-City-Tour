026d3e2e690665fdb6b053c322ef52b850efd5fb
saifdev1642 <saifdev1642@gmail.com>
2020-08-11 23:42:44 +0600

    tour component

--- src/components/tour/tour.js
+++ src/components/tour/tour.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react'
 
 export default class Tour extends Component {
     render() {
+        const {id, city, img, name, info} = this.props.tour;
+        const { removeTour } = this.props;
         return (
-            <div>
-                <h3>Hello From Tour</h3>
-            </div>
+            <article className="tour">
+                <div className="img-container">
+                    <img src={img} alt=""/>
+                    <span className="close-btn">
+                        <i className="fas fa-window-close" />
+                    </span>
+                </div>
+                <div className="tour-info">
+                    <h3>{city}</h3>
+                    <h3>{name}</h3>
+                    info <span><i className="fas fa-caret-square-down"></i></span>
+                    <p>{info}</p>
+                </div>
+            </article>
         )
     }
 }
